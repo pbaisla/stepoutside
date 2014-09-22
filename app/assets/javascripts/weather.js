70637264b675b58a645dfdc57f339e1629eb3247
pbaisla <prashant_baisla@yahoo.com>
2014-09-22 12:23:36 +0530

    Remove hardcoded api key

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -5,7 +5,7 @@ function insertReply(content) {
 
 function getWeather() {
 	var location = document.getElementById('search').value;
-	var jsonURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=aaaac8df0843c32c3fdb3d915acec2a4&units=metric&callback=insertReply';
+	var jsonURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + apikey + '&units=metric&callback=insertReply';
 	var script = document.createElement('script');
 	script.src = jsonURL;
 	document.body.appendChild(script);
