a295c47dc924fadfdc6d162adbe0aa1d28295b50
pbaisla <prashant_baisla@yahoo.com>
2014-09-22 12:02:35 +0530

    Add weather.js and basic temperature search

---
+++ app/assets/javascripts/weather.js
@@ -0,0 +1,14 @@
+function insertReply(content) {
+	console.log('monkey');
+	var temp = document.getElementById('temperature');
+	temp.innerHTML = content.main.temp;
+}
+
+function getWeather() {
+	var location = document.getElementById('search').value;
+	var jsonURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=aaaac8df0843c32c3fdb3d915acec2a4&units=metric&callback=insertReply';
+	var script = document.createElement('script');
+	script.src = jsonURL;
+	document.body.appendChild(script);
+	script.parentNode.removeChild(script);
+}
