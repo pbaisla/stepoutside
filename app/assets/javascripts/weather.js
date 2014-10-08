0214b280d78def0356d1b4cafe42ae8e08bb3546
pbaisla <prashant_baisla@yahoo.com>
2014-10-08 23:53:55 +0530

    Add times and icon

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,10 +1,12 @@
 $(function() {
+	var cleartime;
 	var addresspicker = $( "#search" ).addresspicker({
 	  updateCallback: showCallback
 	});
 
 	function showCallback(geocodeResult, parsedGeocodeResult){
 		console.log("In showCallback");
+		clearTimeout(cleartime);
 	  	var latitude = parsedGeocodeResult.lat;
 	  	var longitude = parsedGeocodeResult.lng;
 	  	$.ajax({
@@ -19,6 +21,8 @@ $(function() {
 				$('#sunrise').text(response.daily.sunriseTime);
 				$('#sunset').text(response.daily.sunsetTime);
 				$('#icon').text(response.currently.icon);
+				console.log("/images/icons/" + response.currently.icon + ".png");
+				$('#weather-img').attr("src", "/images/icons/" + response.currently.icon + ".png");
 				$('#windspeed').text(response.currently.windSpeed);
 				$('#cloudcover').text(response.currently.cloudCover);
 				$('#humidity').text(response.currently.humidity);
@@ -31,7 +35,23 @@ $(function() {
 				$('#latitude').text(response.latitude);
 				$('#longitude').text(response.longitude);
 				$('#timezone').text(response.timezone);
+				$('#localtimezone').text( -1 * (new Date).getTimezoneOffset()/60);
 				$('#offset').text(response.offset);
+				$('#currenttime').text(response.currently.time);
+
+				function clock() {
+					var current = new Date();
+					$('#localtime').text(current.toLocaleTimeString());
+					var currenttime = current.getTime();
+					var offset = current.getTimezoneOffset() * 60000;
+					var utctime = currenttime + offset;
+					var locationTime = utctime + (3600000 * response.offset);
+					console.log(utctime+ " " + response.offset +" "+locationTime);
+					var location = new Date(locationTime);
+					$('#currenttime').text(location.toLocaleTimeString());
+					cleartime = setTimeout(clock, 1000);
+				}
+				clock();
 				console.log( response.currently );
 				console.log( response ); // server response
 			}
